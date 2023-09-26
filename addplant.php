e70ac77a60b980ee82abbd486b613661df7bffe4
odics <odics@vcu.edu>
2023-09-26 14:42:38 +0100

    Add form html and form validator

--- addplant.php
+++ addplant.php
@@ -1,1 +1,79 @@
 <?php
+require_once 'components/navbar.php';
+
+echo '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Plant Manager</title>
+
+    <meta name="description" content="Plant manager app." />
+    <meta name="author" content="Severin Odic" />
+
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192" />
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="apple-touch-icon" href="images/favicon.png" />
+
+    <script defer src="js/validator.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Nunito:300,400,700&display=swap"
+          />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+          />
+  </head>
+  <body>'
+    . renderNavBar() .
+    '<div class="form-container">
+        <div class="form-card">
+        <div class="form-header">
+        Add New Plant
+        </div>
+        <div class="form-card-body">
+         <form class="plant-form" id="new-plant-form" action="process-new-plant.php">
+         <div class="input-group-container">
+            <div class="input-group">
+                <label for="plant-name" id="name-label">Plant name</label>
+                <input type="text" id="plant-name" placeholder="e.g. Tomato">
+            </div>
+            <div class="input-group">
+                <label for="plant-cultivar" id="cultivar-label">Plant cultivar</label>
+                <input type="text" id="plant-cultivar" placeholder="e.g. Cherry">
+            </div>
+         </div>
+         <div class="input-group-container">
+            <div class="input-group">
+                <label for="date-planted" id="date-planted-label">Date planted</label>
+                <input type="date" id="date-planted">
+            </div>
+            <div class="input-group">
+                <label for="date-planted" id="proj-harvest-label">Projected harvest</label>
+                <input type="date" id="projected-harvest">
+            </div>
+         </div>
+         <div class="input-group-container">
+         <div class="input-group">
+                <label for="plant-img" id="img-url-label">Image URL</label>
+                <input type="text" id="plant-img" placeholder="Leave blank for default picture">
+            </div>
+         </div>
+         <div class="button-group">
+            <button class="btn-primary" id="submit-new-plant">Submit</button>
+            <button class="btn-secondary">Cancel</button>
+        </div>
+         </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>';
