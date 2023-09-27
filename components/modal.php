54d6930713128089d92e55487e801df7c0da6534
odics <odics@vcu.edu>
2023-09-27 14:57:49 +0100

    Refarctor modal

--- components/modal.php
+++ components/modal.php
@@ -1,29 +1,8 @@
 <?php
 
-function renderModal(string $modalTitle = '', string $modalBody = ''): string
+function renderModal(string $modalTitle, string $modalBody): string
 {
-    if (!$modalTitle && !$modalBody) {
-        return '
-    <div class="modal-overlay" id="modal-overlay">
-        <div class="modal" id="modal">
-            <div class="form-card new-plant">
-                <div class="form-header">
-                </div>
-                <div class="form-card-body">
-                <div class="card-text"></div>
-                    <div class="plant-type-buttons">
-                        <form method="POST" action="forms/delete-plant.php" id="delete-plant">
-                            <input type="hidden" value="test" name ="plantID" id="plantID">
-                            <button class="btn-primary" id="delete-plant">Delete</button>
-                            <button class="btn-secondary" id="modal-cancel">Cancel</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>';
-    } else {
-        return '
+    return '
     <div class="modal-overlay" id="modal-overlay"></div>
         <div class="modal" id="modal">
             <div class="form-card new-plant">
@@ -35,5 +14,4 @@ function renderModal(string $modalTitle = '', string $modalBody = ''): string
             '</div>
             </div>
         </div>';
-    }
 }
