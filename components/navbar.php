55fbc189d7ba126766dec1c08829831a6f02eb16
odics <odics@vcu.edu>
2023-09-28 09:38:35 +0100

    Initial search page layout

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav-container">
-        <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+        <a href="search.php"><i class="fa-solid fa-magnifying-glass"></i></a>
         <a href="index.php"><i class="fa-solid fa-house home-button"></i></a>
         <a href="addplant.php"><i class="fa-solid fa-plus"></i></a>
     </div>
